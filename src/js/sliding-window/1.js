903ee517bbd775b54c2399367c9d03aee6b1ba0f
Parth Patel <akshup66@gmail.com>
2026-08-17 23:31:08 -0400

    implement maxSubarraySum function with sliding window technique

--- src/js/sliding-window/1.js
+++ src/js/sliding-window/1.js
@@ -7,4 +7,22 @@
  *
  */
 
-function maxSubarraySum(nums, k) {}
+function maxSubarraySum(nums, k) {
+  let windowSum = 0,
+    maxSubArraySum = 0;
+
+  for (let j = 0; j < k; j++) windowSum += nums[j];
+
+  // Doing this prevents the first window value from being thrown away. (Take first window value into account)
+  maxSubArraySum = windowSum;
+
+  for (let i = 0; i + k < nums.length; i++) {
+    windowSum = windowSum - nums[i] + nums[k + i];
+    maxSubArraySum = Math.max(windowSum, maxSubArraySum);
+  }
+
+  return maxSubArraySum;
+}
+
+console.log("MAX subarray sum: ", maxSubarraySum([2, 1, 5, 1, 3, 2], 3));
+console.log("MAX subarray sum: ", maxSubarraySum([1, 4, 5], 3));
